5af7b674f97f2bcf06dd0e3a37f721c8bfbe5285
Sung <25SungC@badinhs.org>
2025-04-16 14:28:15 -0400

    Wrote some steps

--- Problem1.js
+++ Problem1.js
@@ -1,13 +1,17 @@
+/*
+1. Create Prompt to generate Height
+2. Use generated Height to create pyradmine
+3. Figure out how to create Loop function 
+*/
 let num
 
-function(num){
+function num() {
     +prompt("Insert Maximum Height",);
 }
 
 
 while (i <= num){
     String("#", num)
-    console.log(String)
-    i++;
 
+    console.log()
 }
