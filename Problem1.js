52500c8ebf30aaffe656492fc2a69e37bc42ae63
Sung <25SungC@badinhs.org>
2025-04-28 13:53:26 -0400

    added more

--- Problem1.js
+++ Problem1.js
@@ -1,17 +1,58 @@
-/*
-1. Create Prompt to generate Height
-2. Use generated Height to create pyradmine
-3. Figure out how to create Loop function 
-*/
-let num
+// Mario Problem //
 
-function num() {
-    +prompt("Insert Maximum Height",);
+
+let Mario = +prompt("How Much P") 
+let Luigi = +prompt("How much G")
+
+
+// function build(Mario) {
+//     for (let i = 0; i < Mario ; i++){
+//         let row = "";
+//         row = row + " ";
+//         row = row + "#"
+//         console.log(row)
+//     }
+// }
+// function builder(Mario) {
+//     build(Mario);
+//     let row2 = row
+//     row2 = row2 + "#";
+//     // row2.slice(i, Mario);
+//     console.log (row2);
+// }
+
+// for(let i = 0; i < Mario; i++){
+//     build(Mario);
+//     console.log(build(Mario))
+// }
+
+// function buildest(Luigi) {
+//     let  row3 = "  ";
+//     for (let i = 0; i < Mario ; i++){
+//         row3 = row3 + " "
+//     }
+//     row3 = row3 + "#";
+//     console.log (row3);
+// }
+
+// buildest(Luigi);
+// console.log(buildest(Luigi));
+
+function Repeat (Mario){
+    for (let i = 0; i < Mario; i++){
+    let row = "";
+    row = row + " ";
+    console.log(row);
+    }
+};
+
+function Repeat2 (Mario){
+    for (let i = Mario; i <= 0; i--){
+    Repeat (Mario);
+    row = row + "#";
+    console.log(row);
+    }
 }
 
-
-while (i <= num){
-    String("#", num)
-
-    console.log()
-}
+// Repeat (Mario);
+Repeat2 (Mario);
