15d4b614ac8bc90da998f481ef7577afb2d15b97
Sung <25SungC@badinhs.org>
2025-05-14 13:12:36 -0400

    Testing Idea

---
+++ Problem2.js
@@ -0,0 +1,9 @@
+// Prime Factor:
+
+let Num = +prompt ("Random Number NOW"); // 600851475143
+let Num2 = 600851475143
+for ( i = 1; i <= Num2; i++){
+    if (Num2 % i == 0){
+        return i;
+    }
+}
